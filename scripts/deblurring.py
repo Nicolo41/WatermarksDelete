2856df6d34ff337199452ff39ba9a983a8b4e813
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-12-19 07:59:56 +0100

    clean deblurring

--- scripts/deblurring.py
+++ scripts/deblurring.py
@@ -15,22 +15,17 @@ def postprocess_image(image):
     return image
 
 model = tf.keras.models.load_model("models/autoencoder_model.keras")
-
 image_path = "data/w_remove_dataset/80a05f5b-59d8-48d1-89b0-00ac27356bed.png" 
-
 
 blurry_image = preprocess_image(image_path)
 
-
 deblurred_image = model.predict(blurry_image)
-
-
 deblurred_image = postprocess_image(deblurred_image)
-
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 original_image = cv2.imread(image_path)
 original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
 ax[0].imshow(original_image)
 ax[0].set_title("Image Floue")
 ax[0].axis("off")
@@ -42,6 +37,5 @@ ax[1].axis("off")
 plt.tight_layout()
 plt.show()
 
-
-# cv2.imwrite("deblurred_output.png", cv2.cvtColor(deblurred_image, cv2.COLOR_RGB2BGR))
 print("Défloutage terminé, image sauvegardée sous 'deblurred_output.png'.")
+# cv2.imwrite("deblurred_output.png", cv2.cvtColor(deblurred_image, cv2.COLOR_RGB2BGR))
